abb89c6db24586bc8513e62d8541d07d03bb1373
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-31 16:56:04 -0400

    finished second list

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
- const firstList = document.querySelectorAll('ol li')
+const firstList = document.querySelectorAll('ol li')
+const secondList = document.querySelectorAll('ul li')
+const images = document.querySelectorAll('#row *')
 
- function lineThrough(event){
+function lineThrough(event){
     event.target.style.textDecoration = 'line-through'
  }
-for(let i = 0; i < firstList.length; i++){
+
+ for(let i = 0; i < firstList.length; i++){
     firstList[i].addEventListener('click',lineThrough)
 }
+
+function dirkFadeAway(event){
+    event.target.style.opacity = '0'
+    
+}
+
+for(let i = 0; i < firstList.length; i++){
+    secondList[i].addEventListener('click', dirkFadeAway)
+}
+
+
+function foldAway(event){
+    event.target.style.height = '1px'
+}
+
+for(let i = 0; i < firstList.length; i++){
+    images[i].addEventListener('click', foldAway)
+}
+
