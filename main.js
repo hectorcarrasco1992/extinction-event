ee5a76834891640b10497d6dbb070c842bea282e
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-31 17:27:30 -0400

    finished meteor button

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const firstList = document.querySelectorAll('ol li')
 const secondList = document.querySelectorAll('ul li')
-const images = document.querySelectorAll('#row *')
+const images = document.querySelectorAll('img')
+const explode = document.querySelector('#toggle').addEventListener('click', boom)
 
 function lineThrough(event){
     event.target.style.textDecoration = 'line-through'
@@ -21,10 +22,28 @@ for(let i = 0; i < firstList.length; i++){
 
 
 function foldAway(event){
-    event.target.style.height = '1px'
+    event.target.style.width = '0px'
 }
 
 for(let i = 0; i < firstList.length; i++){
     images[i].addEventListener('click', foldAway)
 }
+// explode.addEventListener('click', boom)
 
+function boom(){
+    for(let i = 0; i < images.length; i++){
+        images[i].style.width = '0px'
+
+    }
+
+    for (let i = 0; i < secondList.length; i++){
+        secondList[i].style.opacity = '0'
+    }
+
+    for (let i = 0; i < firstList.length; i++){
+        firstList[i].style.textDecoration = 'line-through'
+    }
+
+    
+    
+}
